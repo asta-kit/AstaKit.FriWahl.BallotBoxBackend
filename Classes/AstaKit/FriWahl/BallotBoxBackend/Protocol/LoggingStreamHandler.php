dd10f3aa030cf2d8271688f3eaead2f87ee0c3a6
Andreas Wolf <andreas.wolf@usta.de>
2014-07-06 16:18:40 +0200

    [FEATURE] Log memory usage in logging stream handler

--- Classes/AstaKit/FriWahl/BallotBoxBackend/Protocol/LoggingStreamHandler.php
+++ Classes/AstaKit/FriWahl/BallotBoxBackend/Protocol/LoggingStreamHandler.php
@@ -28,6 +28,13 @@ class LoggingStreamHandler implements StreamHandler {
 	 */
 	protected $logger;
 
+	/**
+	 * If set, the memory consumption of the current process is logged after each command.
+	 *
+	 * @var bool
+	 */
+	protected $logMemoryConsumption = FALSE;
+
 
 	/**
 	 * @param StreamHandler $originalHandler
@@ -40,16 +47,27 @@ class LoggingStreamHandler implements StreamHandler {
 		$this->originalHandler->setLineEnding($lineEnding);
 	}
 
+	protected function convertSize($size) {
+		// see http://de3.php.net/manual/en/function.memory-get-usage.php#96280
+		$units = array('B','KiB','MiB', 'GiB');
+		$power = (int)floor(log($size, 1024));
+		return @round($size / pow(1024, $power), 2) . ' ' . $units[$power];
+	}
+
 	public function readLine() {
+		if ($this->logMemoryConsumption) {
+			$this->logger->log('Mem usage (current/peak): '
+				. $this->convertSize(memory_get_usage()) . '/' . $this->convertSize(memory_get_peak_usage()));
+		}
 		$line = $this->originalHandler->readLine();
-		$this->logger->log('<' . $line);
+		$this->logger->log('< ' . trim($line));
 
 		return $line;
 	}
 
 	public function writeLine($contents) {
 		$this->originalHandler->writeLine($contents);
-		$this->logger->log('>' . $contents);
+		$this->logger->log('> ' . $contents);
 	}
 
 	public function close() {
